c7a1e5f9a51a42390b64cfe8249df4b8edd7fa29
Spencer Alger <spencer@spenceralger.com>
2013-12-20 09:34:06 -0700

    Fixed the bulk request formatting

--- js/elasticsearch.js
+++ js/elasticsearch.js
@@ -1,4 +1,4 @@
-/*! elasticsearch - v1.0.2 - 2013-12-19
+/*! elasticsearch - v1.0.2 - 2013-12-20
  * http://elasticsearch.github.io/elasticsearch-js/
  * Copyright (c) 2013 Elasticsearch BV; Licensed Apache 2.0 */
 !function(e){"object"==typeof exports?module.exports=e():"function"==typeof define&&define.amd?define(e):"undefined"!=typeof window?window.elasticsearch=e():"undefined"!=typeof global?global.elasticsearch=e():"undefined"!=typeof self&&(self.elasticsearch=e())}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
