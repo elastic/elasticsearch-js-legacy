317b77a4261a0f10008f7cbbf52ea481666d7a45
spalger <spalger@users.noreply.github.com>
2020-03-26 15:19:01 -0700

    [api] regenerate

--- src/lib/apis/browser_index.js
+++ src/lib/apis/browser_index.js
@@ -1,8 +1,10 @@
 module.exports = {
   '_default': require('./7_6'),
   '7.6': require('./7_6'),
-  '6.8': require('./6_8'),
-  '5.6': require('./5_6'),
+  '7.5': require('./7_5'),
+  '7.4': require('./7_4'),
+  '7.3': require('./7_3'),
+  '7.2': require('./7_2'),
   '7.7': require('./7_7'),
   '7.x': require('./7_x'),
   'master': require('./master')
